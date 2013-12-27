ee938cf50cb6df225b3c27adfb821dedd13318c2
Filipe Cardeira <filipe@vitflodhast.com>
2013-12-27 18:20:17 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,509 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>StartUp Groovy | We help entrepreneurs to turn their ideas into a business</title>
+<meta charset="UTF-8" />
+<link href="Images/favicon.ico" rel="icon" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="css/index.css"/>
+<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+
+<script src='Js/smooth.js'></script>
+</head>
+
+	<body>
+
+
+		<div id="header">
+			
+			<div id="header-inner">
+		        	
+			        <a href="#"><img class="logo" src="http://i.imgur.com/Xod33W3.jpg"></a>
+			        
+			        <ul id="menu">
+			        	
+			        	<li> <a href="#"> LOCATIONS </a> 
+			        	
+				        	<ul class="sub-menu">
+				        		<li><a href="#"> LISBON  </a></li>	
+				        		<li><a href="#"> BUDAPESTE - coming soon </a></li>
+				        		<li><a href="#"> BUENOS AIRES - coming soon </a></li>
+				        	</ul> 
+			        	
+			        	</li>
+			        	
+			        	<li class="yellow"><a href="#">WHY I TRAVEL</a></li>
+						<li class="red"><a href="#">LOGIN</a></li>
+						
+
+						<!-- AddThis Follow BEGIN -->
+						<div class="addthis_toolbox addthis_32x32_style addthis_default_style" style="float:right;">
+						<a class="addthis_button_facebook_follow" addthis:userid="startupgroovy"  style="line-height: 49px; margin: 0 0 02px; padding-top: 10px;"></a>
+						<a class="addthis_button_twitter_follow" addthis:userid="startupgroovy"   style="line-height: 49px; padding-top: 10px;"></a>
+						<a class="addthis_button_instagram_follow" addthis:userid="startupgroovy" style="line-height: 49px; padding-top: 10px;"></a>
+						</div>
+						<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5215cb2357b4644d"></script>
+						<!-- AddThis Follow END -->
+						
+					</ul>
+
+			 	</div>
+			 	
+
+			</div>
+
+
+		<div id="intro_top">
+
+			<div class="content-slider">
+
+					<div class="full-bg-image" style="background-image: url(http://i.imgur.com/orcbZqp.jpg); opacity: 1;"> </div>
+
+					<div class="section-box">
+
+						<ul class="ul_intro">
+							<li>
+								<div class="ul_intro_col">
+									<h1 class="title">We help entrepreneurs turn their ideas into a business!</h1>
+								</div>
+							</li>
+						</ul>
+
+					</div>
+					
+					<div class="wrap-arrow">
+					
+						<div class="arrow"><a href="#intro_content"><img src="http://s7.postimg.org/elib4gqsn/seta.png?noCache=1385255308" alt="" /></a></div>
+					
+					</div>
+				
+			</div>
+
+		</div>
+
+		<div id="intro_content">
+
+			<div class="content">
+
+				<h2> We give you the edge </h2>
+
+				<div class="wrap_content">
+
+					<div class="section_box">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/house.png">
+
+						</div>
+
+						<div class="description">
+
+							<h3>accommodation</h3>
+
+							<p>Central. Connected. Close to bars and cafes, amenities, metro, trams and walking distance to work.</p>
+
+						</div>
+
+					</div>
+
+					<div class="section_box">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/desk.png">
+
+						</div>
+
+						<div class="description">
+
+							<h3>Office space</h3>
+
+							<p>We set you up in a vibrant StartUp co-working space, with a strong community, looked after by members.</p>
+
+						</div>
+
+					</div>
+
+					<div class="section_box">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/ballons.png">
+
+						</div>
+
+						<div class="description">
+
+							<h3>Coaching</h3>
+
+							<p>The “Survival Toolbox for Entrepreneurs”: pitching events, workshops on critical subjects given by experienced entrepreneurs and much more.</p>
+
+						</div>
+
+					</div>
+
+					<div class="section_box">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/sienna.png">
+
+						</div>
+
+						<div class="description">
+
+							<h3>Support</h3>
+
+							<p>We have an in-house team of developers to help you grow your StartUp with no strings attached.</p>
+
+						</div>
+
+					</div>
+
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+		<div id="info_content">
+
+			<div class="wrap_info_text">
+
+				<h2>Let's fly! Check out our locations</h2>
+				<p>or get in touch with us.</p>
+
+			</div>
+
+		</div>
+
+		<div id="testimonial">
+
+			<div id="testimonial_container">
+
+				<div class="section">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/ballons.png">
+
+						</div>
+
+						<blockquote><p>The weather was better</p></blockquote>
+
+						<div class="description">
+
+							<h2>Will Riakos</h2>
+
+							<p>Entrepreneur / <a href="http://twitter.com">Twitter</a></p>
+
+						</div>
+
+						
+
+				</div>
+
+
+
+				<div class="section">
+
+						<div class="img">
+
+							<img src="http://ytahd.com/groovy/wp-content/uploads/2013/12/sienna1-150x150.png">
+
+						</div>
+
+						<blockquote><p>The weather was better</p></blockquote>
+
+						<div class="description">
+
+							<h2>Will Riakos</h2>
+
+							<p>Entrepreneur / <a href="http://twitter.com">Twitter</a></p>
+
+						</div>	
+
+				</div>
+
+
+			</div>
+
+
+		</div>
+				
+				
+		<div id="location">
+			
+			<div id="intro">
+
+	                 <div class="video">
+
+	                     <iframe class="video_src" width="100%" height="350px" src="//player.vimeo.com/video/79745135" frameborder="0"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+	                 </div>
+
+
+	                 <div class="promosg">
+
+	                    <div class="wrap_text">
+
+	                         <h1>US$250/WEEK</h1>
+
+	                         <p>1 BEDDER, DESK AT A VIBRANT
+	                         1 BEDDER, DESK AT A VIBRANT</br>
+	                         1 BEDDER, DESK AT A VIBRANT</br>
+	                         PICK UP</p>
+
+	                         <br>
+
+	                         <h1>APPLY NOW</h1>
+
+	                         <p>PROGRAM STARTS ON 3RD FB</br>
+	                         ONLY 19 PLACES LEFT</p>
+
+<?php 
+						$count = $_SESSION['count'];
+						if($count == 0)
+							{
+?>
+	                         <div id="wrap_leadform">
+
+	                         	 <form method="post" action="send.php" class="leadform">
+
+	                                <input class="input_email" type="email" name="email" placeholder="name@hotmail.com"/>
+
+	                                <input class="hitme" type="submit" name="submit" value="GO" />
+
+	                            </form> 
+
+	                        </div>
+<?php
+	                        }
+	                    else
+	                    {
+?>
+							 <div id="wrap_leadform">
+							 
+							 <script>
+							 $(function() { 
+									scrollTop: $('#wrap_leadform', 4000).offset().top
+							});
+							 </script>
+
+							 	<img src="http://i.imgur.com/cpEtdUY.png" style="margin-top:20px;" width="350" align="center" alt="Sucess!" />
+
+							 </div>
+<?php
+	                    }
+?>
+
+	                    </div>
+
+	                 </div>
+
+	            </div>
+
+		</div>
+
+		<div id="wrap_content_local_team">
+
+			<div id="content_local_team">
+
+	            <div id="main">
+				
+
+	                <div class="main_text"> 
+
+	                        <h1> STARTUP LISBON </h1>
+	                        <p>insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text 
+	                        </p>
+
+	                </div>
+
+
+	                <div id="acommodation">
+
+	                	 <div class="main_text"> 
+
+	                        <h1> ACOMMODATION </h1>
+	                        <p>insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text 
+	                        </p>
+
+	                	 </div>
+
+	                	 <img src="http://i.imgur.com/e0nao2i.jpg" alt="">
+
+
+	                </div>
+
+
+	                <div id="office">
+
+	                	 <div class="main_text"> 
+
+	                        <h1> OFFICE SPACE </h1>
+	                        <p>insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text 
+	                        </p>
+
+	                	 </div>
+
+	                	 <img src="http://i.imgur.com/e0nao2i.jpg" alt="">
+
+
+	                </div>
+
+	                <div id="program">
+
+	                	 <div class="main_text"> 
+
+	                        <h1> PROGRAM </h1>
+	                        <p>insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text insert text insert text insert text insert text 
+	                        	insert text insert text insert text insert text insert text insert text 
+	                        </p>
+
+	                	 </div>
+
+	                	 <img src="http://i.imgur.com/e0nao2i.jpg" alt="">
+
+
+	                </div>
+
+	                <div id="wrap_local_team">
+					
+					
+		                <div class="main_text_team"> 
+
+		                        <h1>LOCAL TEAM </h1>
+
+		                </div>
+
+	                    <div class="local">
+	                          <div class="team_img"><img src="http://i.imgur.com/e0nao2i.jpg" alt=""></div>
+	                          <div class="team_info"><h3>Sergio Medes</h3></div>
+	                          <div class="func"> <h3>Creative Director</h3> </div>
+	                    </div>
+
+	                    <div class="local">
+	                          <div class="team_img"><img src="http://i.imgur.com/e0nao2i.jpg" alt=""></div>
+	                          <div class="team_info"><h3>Sergio Medes</h3></div>
+	                          <div class="func"> <h3>Creative Director</h3> </div>
+	                    </div>
+
+	                    <div class="local">
+	                          <div class="team_img"><img src="http://i.imgur.com/e0nao2i.jpg" alt=""></div>
+	                          <div class="team_info"><h3>Sergio Medes</h3></div>
+	                          <div class="func"> <h3>Creative Director</h3> </div>
+	                    </div>
+
+	                    <div class="local">
+	                          <div class="team_img"><img src="http://i.imgur.com/e0nao2i.jpg" alt=""></div>
+	                          <div class="team_info"><h3>Sergio Medes</h3></div>
+	                          <div class="func"> <h3>Creative Director</h3> </div>
+	                    </div>
+
+	                </div>  
+
+	        	</div>
+
+	            <div id="navsidebar">
+
+	               <div class="wrap_sidebar">
+
+			           <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+
+			             <a class="addthis_button_facebook"></a>
+			             <a class="addthis_button_twitter"></a>
+			             <a class="addthis_button_email"></a>
+			             <a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
+
+			           </div>
+
+
+		               <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+		               <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5215cb2357b4644d"></script>
+
+	                   <br>
+
+	                   <ul id="sidebar">
+	                     <li><a href="#main">STARTUP LISBON</a></li>
+	                     <li><a href="#acommodation">ACOMMODATION</a></li>
+	                     <li><a href="#office">OFFICE SPACE</a></li>
+	                     <li><a href="#program">OUR PROGRAM</a></li>
+	                     <li><a href="#wrap_local_team">LOCAL TEAM</a></li>
+	                   </ul>
+
+	                    <div id="wrap_leadform">
+
+	                             <!-- Begin MailChimp Signup Form -->
+								<div id="mc_embed_signup">
+								<form action="http://startupgroovy.us3.list-manage.com/subscribe/post?u=91315b87b04d6ca5ba52831c2&amp;id=6796b91c6c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+									
+								<div class="mc-field-group">
+									<label for="mce-EMAIL"> Subscribe to ours newsletter </label>
+									<input type="email" value="" name="EMAIL" class="required email" placeholder="name@hotmail.com" id="mce-EMAIL">
+								</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+								    <div style="position: absolute; left: -5000px;"><input type="text" name="b_91315b87b04d6ca5ba52831c2_6796b91c6c" value=""></div>
+									<div class="clear"><input type="submit" value="Ok" name="subscribe" id="mc-embedded-subscribe" class="button hitme"></div>
+								</form>
+								</div>
+
+								<!--End mc_embed_signup-->
+
+
+	                    </div>
+
+	               </div>
+
+	            </div>
+
+	   		</div>
+
+	   	</div>
+
+		<!-- Footer -->
+
+		<div id="footer">
+
+			<div class="text">
+				&copy; 2013 Startup groovy. All Rights Reserved
+			</div>
+
+		<div>
+
+		<div style="background: url(Images/fish.jpg) repeat; margin: -3px auto;"> 
+
+			<img src="Images/fish.jpg" width="auto" style="min-height:300px; background-repeat: repeat;" height="auto" alt="img" />
+
+		</div>
+		
+		<!-- Script para rodar sticky nav -->
+		 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+		
+			<script src="Js/sticky.js"></script>
+		
+			<script>
+			(function($) {
+				$.lockfixed("#navsidebar .wrap_sidebar",{offset: {top: 60, bottom: 610}});
+				
+			})(jQuery);
+			</script>
+
+	</body>
+
+</html>
